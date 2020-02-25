a5e6c8cd495e628d2b23a74b7a290cff530a4546
cjybyjk <cjybyjk@gmail.com>
2020-02-25 22:27:08 +0800

    config: add exTHmUI Theme package

--- config/common.mk
+++ config/common.mk
@@ -160,6 +160,10 @@ PRODUCT_PACKAGES += \
     APlayer \
     Lawnchair
 
+# exTHmUI Theme
+PRODUCT_PACKAGES += \
+    DefaultTheme
+
 # MiPush
 ifneq ($(WITHOUT_MIPUSH),true)
 PRODUCT_PACKAGES += \
